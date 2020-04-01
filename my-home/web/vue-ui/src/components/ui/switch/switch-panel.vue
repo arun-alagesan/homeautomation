7dc64a2868886231f4cdcac6e5585bd546650a0f
Arun <arun.alagesan@gmail.com>
2020-04-01 13:05:47 +0530

    Draft

---
+++ my-home/web/vue-ui/src/components/ui/switch/switch-panel.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="switch-panel" >
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        
+        name : 'SwitchPanel',
+
+        created (){
+            console.log("SwitchPanel - Created")
+        }
+
+    }
+</script>
+
+<style lang="scss" scoped>
+    .switch-panel{
+        margin:1em;
+        padding :0; 
+        border:1px solid red;
+        display: flex;
+        flex-flow: row wrap;
+        justify-content:space-evenly;
+    }
+</style>
