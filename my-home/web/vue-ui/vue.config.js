734774ed1b1d8ca09702beb93092fb0a716bb725
Arun Alagesan <arun.alagesan@gmail.com>
2020-04-27 21:56:35 +0530

    Added the logic to read appversion from package.json

--- my-home/web/vue-ui/vue.config.js
+++ my-home/web/vue-ui/vue.config.js
@@ -1,7 +1,24 @@
+/* eslint-disable */
+
+let webpack = require('webpack');
+
+const version = require("./package.json").version || 0;
+process.env.VUE_APP_VERSION = version;
+
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production-mac'
       ? '/iot/my-home/'
-      : process.env.NODE_ENV === 'production' ? '/my-home-vue-ui/' : '/'
+      : process.env.NODE_ENV === 'production' ? '/my-home-vue-ui/' : '/',
+
+    configureWebpack :{
+      plugins:[
+        new webpack.DefinePlugin({
+          'process.env' : {
+            PACKAGE_VERSION : '"' + version + '"'
+          }
+        })
+      ]
+    }
 
     /* publicPath: process.env.NODE_ENV === 'production'
       ? '/iot/my-home/'
